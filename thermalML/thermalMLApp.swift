5e4913ae5534b4da3d6daa9ee2f68fea2481c074
Max <renkostlab@gmail.com>
2023-11-20 17:22:35 +0100

    Initial Commit

---
+++ thermalML/thermalMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  thermalMLApp.swift
+//  thermalML
+//
+//  Created by Smart Manufacturing and Robotics on 20.11.23.
+//
+
+import SwiftUI
+
+@main
+struct thermalMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
